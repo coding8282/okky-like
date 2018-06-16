3d9d250bf5181daa1c0d19b07aa343ba9869a0bf
hyunsu <coding8282@gmail.com>
2018-06-16 18:16:08 +0900

    클래스 field를 lombok을 @FieldDefaults를 이용해서 단순하게 함

--- src/main/java/org/okky/like/domain/model/ArticleLike.java
+++ src/main/java/org/okky/like/domain/model/ArticleLike.java
@@ -3,6 +3,7 @@ package org.okky.like.domain.model;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.okky.share.domain.Aggregate;
 import org.okky.share.domain.AssertionConcern;
 import org.springframework.data.annotation.CreatedDate;
@@ -10,10 +11,12 @@ import org.springframework.data.annotation.CreatedDate;
 import javax.persistence.*;
 import java.util.UUID;
 
+import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
 
 @NoArgsConstructor(access = PROTECTED)
 @EqualsAndHashCode(of = "id", callSuper = false)
+@FieldDefaults(level = PRIVATE)
 @Getter
 @Entity
 @Table(uniqueConstraints = {
@@ -24,17 +27,17 @@ import static lombok.AccessLevel.PROTECTED;
 public class ArticleLike implements Aggregate {
     @Id
     @Column(length = 50)
-    private String id;
+    String id;
 
     @Column(name = "ARTICLE_ID", nullable = false, length = 50)
-    private String articleId;
+    String articleId;
 
     @Column(name = "LIKER_ID", nullable = false, length = 50)
-    private String likerId;
+    String likerId;
 
     @CreatedDate
     @Column(nullable = false, updatable = false, columnDefinition = "BIGINT UNSIGNED")
-    private long likedOn;
+    long likedOn;
 
     public ArticleLike(String articleId, String likerId) {
         setId("al-" + UUID.randomUUID().toString().replaceAll("-", "").substring(0, 15));
