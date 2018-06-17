8c427180806d2b59fc685c4e592e81ddd7495a22
hyunsu <coding8282@gmail.com>
2018-06-18 04:01:02 +0900

    불필요한 선언을 발견하여 삭제

--- src/main/java/org/okky/like/domain/repository/ArticleLikeRepository.java
+++ src/main/java/org/okky/like/domain/repository/ArticleLikeRepository.java
@@ -2,14 +2,13 @@ package org.okky.like.domain.repository;
 
 import org.okky.like.domain.model.ArticleLike;
 import org.springframework.data.repository.RepositoryDefinition;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 @RepositoryDefinition(domainClass = ArticleLike.class, idClass = String.class)
 public interface ArticleLikeRepository {
     void save(ArticleLike like);
-    Optional<ArticleLike> findByArticleIdAndLikerId(@Param("articleId") String articleId, @Param("likerId") String likerId);
+    Optional<ArticleLike> findByArticleIdAndLikerId(String articleId, String likerId);
     boolean existsByArticleIdAndLikerId(String articleId, String likerId);
     long countByArticleId(String articleId);
     long countByLikerId(String likerId);
