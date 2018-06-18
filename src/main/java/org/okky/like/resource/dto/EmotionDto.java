73e1e82c5b866305796004c3d590f41e0fc4767f
hyunsu <coding8282@gmail.com>
2018-06-19 00:00:11 +0900

    Emotion DTO 작성. 가장 저수준으로 작동하며 관련 단위테스트에 반영함
    - 튜플에서 컬럼의 위치에 주의해야 함. SQL과 직접적으로 연관되어 있있음 유의해야 함.

---
+++ src/main/java/org/okky/like/resource/dto/EmotionDto.java
@@ -0,0 +1,30 @@
+package org.okky.like.resource.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigInteger;
+
+@AllArgsConstructor
+@Getter
+public class EmotionDto {
+    String id;
+    int totalEmotionCount;
+    int likeCount;
+    int funCount;
+    int thanksCount;
+    int sadCount;
+    int angryCount;
+    String myEmotionType;
+
+    public EmotionDto(Object[] tuple) {
+        // TODO: 2018. 6. 18. @SqlResultSetMapping이 원하는 대로 작동하지 않아 부득이하게 저수준으로 작업함. 자세한 사항은 58ab0f7bfb775d2e2dd960d4058fb9c1045c79b9 참고.
+        this.id = (String) tuple[0];
+        this.totalEmotionCount = ((BigInteger) tuple[1]).intValue();
+        this.likeCount = ((BigInteger) tuple[2]).intValue();
+        this.funCount = ((BigInteger) tuple[3]).intValue();
+        this.thanksCount = ((BigInteger) tuple[4]).intValue();
+        this.sadCount = ((BigInteger) tuple[5]).intValue();
+        this.angryCount = ((BigInteger) tuple[6]).intValue();
+    }
+}
