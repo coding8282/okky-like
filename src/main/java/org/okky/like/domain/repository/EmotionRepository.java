7947e9664e2b1c62b1e8970e6d17a6d18d5c2d1b
hyunsu <coding8282@gmail.com>
2018-06-19 00:42:15 +0900

    EmotionRepository 수정 작성 및 테스트
    - 사용하지 않는 메소드를 삭제하고 새로운 쿼리메소드 추가.
    - findEmotionStatByTargetId는 서브쿼리를 많이 써서 native sql로 작성.

--- src/main/java/org/okky/like/domain/repository/EmotionRepository.java
+++ src/main/java/org/okky/like/domain/repository/EmotionRepository.java
@@ -1,21 +1,32 @@
 package org.okky.like.domain.repository;
 
 import org.okky.like.domain.model.Emotion;
-import org.okky.like.domain.model.EmotionType;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
+
+import static org.okky.like.domain.model.Emotion.GET_EMOTION_QUERY;
 
 @RepositoryDefinition(domainClass = Emotion.class, idClass = String.class)
 public interface EmotionRepository {
     boolean existsByTargetIdAndMemberId(String targetId, String memberId);
-    long countByTargetId(String targetId);
-    long countByMemberIdAndType(String targetId, EmotionType type);
     void save(Emotion emotion);
     Optional<Emotion> findByTargetIdAndMemberId(String targetId, String memberId);
+    @Query(nativeQuery = true, name = GET_EMOTION_QUERY)
+    Object findEmotionStatByTargetId(@Param("targetId") String targetId);
     void deleteByTargetIdAndMemberId(String targetId, String memberId);
 
     default boolean wasAlreadyEmoted(String targetId, String memberId) {
         return existsByTargetIdAndMemberId(targetId, memberId);
+    }
+    default String findEmotionType(String targetId, String memberId) {
+        Emotion emotion = findByTargetIdAndMemberId(targetId, memberId).orElse(null);
+        if (emotion == null) {
+            return null;
+        } else {
+            return emotion.getType().name();
+        }
     }
 }
---
+++ src/test/java/org/okky/like/domain/repository/EmotionRepositoryTest.java
@@ -0,0 +1,140 @@
+package org.okky.like.domain.repository;
+
+import lombok.experimental.FieldDefaults;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.okky.like.TestMother;
+import org.okky.like.domain.model.Emotion;
+import org.okky.like.domain.model.EmotionType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static java.math.BigInteger.valueOf;
+import static lombok.AccessLevel.PRIVATE;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.okky.like.domain.model.EmotionType.*;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@FieldDefaults(level = PRIVATE)
+public class EmotionRepositoryTest extends TestMother {
+    @Autowired
+    EmotionRepository repository;
+
+    @Test
+    public void existsByTargetIdAndMemberId_있는_경우_true() {
+        repository.save(fixture("t1", "m1", LIKE));
+        boolean exists = repository.existsByTargetIdAndMemberId("t1", "m1");
+
+        assertTrue("존재하므로 true를 반환해야 한다.", exists);
+    }
+
+    @Test
+    public void existsByTargetIdAndMemberId_없는_경우_false() {
+        repository.save(fixture("t1", "m1", LIKE));
+        boolean exists = repository.existsByTargetIdAndMemberId("t1", "m9");
+
+        assertFalse("존재하므로 true를 반환해야 한다.", exists);
+    }
+
+    @Test
+    public void findByTargetIdAndMemberId_targetId와_memberId가_같으면_항상_같음() {
+        Emotion emotion = fixture("t1", "m1", LIKE);
+        repository.save(emotion);
+        Emotion retrieved = repository.findByTargetIdAndMemberId("t1", "m1").get();
+
+        assertEquals("둘이 같아야 한다.", emotion, retrieved);
+    }
+
+    @Test
+    public void findByTargetIdAndMemberId_targetId는_같지만_memberId가_다르면_다름() {
+        Emotion emotion = fixture("t1", "m1", LIKE);
+        repository.save(emotion);
+        Optional<Emotion> result = repository.findByTargetIdAndMemberId("t1", "m9");
+
+        assertFalse("존재하지 않는다.", result.isPresent());
+    }
+
+    @Test
+    public void findEmotionStatByTargetId_targetId로_정확히_찾아오는_것을_확인() {
+        repository.save(fixture("t1", "m1", LIKE));
+        repository.save(fixture("t1", "m2", LIKE));
+        repository.save(fixture("t1", "m3", LIKE));
+        repository.save(fixture("t1", "m4", FUN));
+        repository.save(fixture("t1", "m5", ANGRY));
+        repository.save(fixture("t2", "m1", SAD));
+        Object[] tuple = (Object[]) repository.findEmotionStatByTargetId("t1");
+
+        assertThat("targetId는 t1이다.", tuple[0], is("t1"));
+        assertThat("전체 공감표현 개수는 5개이다.", tuple[1], is(valueOf(5)));
+        assertThat("좋아요 개수는 3개이다.", tuple[2], is(valueOf(3)));
+        assertThat("재밌어요 개수는 1개이다.", tuple[3], is(valueOf(1)));
+        assertThat("고마워 개수는 0개이다.", tuple[4], is(valueOf(0)));
+        assertThat("슬퍼 개수는 0개이다.", tuple[5], is(valueOf(0)));
+        assertThat("화나 개수는 1개이다.", tuple[6], is(valueOf(1)));
+    }
+
+    @Test
+    public void findEmotionStatByTargetId_targetId가_없다면_빈_통계를_가져와야_함() {
+        repository.save(fixture("t1", "m1", LIKE));
+        Object[] tuple = (Object[]) repository.findEmotionStatByTargetId("t9");
+
+        assertThat("targetId는 t9이다.", tuple[0], is("t9"));
+        assertThat("없는 target이므로 전체 공감표현 개수는 0개이다.", tuple[1], is(valueOf(0)));
+        assertThat("없는 target이므로 좋아요 개수는 0개이다.", tuple[2], is(valueOf(0)));
+        assertThat("없는 target이므로 재밌어요 개수는 0개이다.", tuple[3], is(valueOf(0)));
+        assertThat("없는 target이므로 고마워 개수는 0개이다.", tuple[4], is(valueOf(0)));
+        assertThat("없는 target이므로 슬퍼 개수는 0개이다.", tuple[5], is(valueOf(0)));
+        assertThat("없는 target이므로 화나 개수는 0개이다.", tuple[6], is(valueOf(0)));
+    }
+
+    @Test
+    public void deleteByTargetIdAndMemberId_삭제_확인() {
+        repository.save(fixture("t1", "m1", LIKE));
+
+        assertTrue("저장 후 존재해야 한다.", repository.existsByTargetIdAndMemberId("t1", "m1"));
+
+        repository.deleteByTargetIdAndMemberId("t1", "m1");
+
+        assertFalse("삭제 후에는 없어야 한다.", repository.existsByTargetIdAndMemberId("t1", "m1"));
+    }
+
+    @Test
+    public void findEmotionType_정확히_가져오는지_확인() {
+        repository.save(fixture("t1", "m1", LIKE));
+
+        String type = repository.findEmotionType("t1", "m1");
+
+        assertEquals("LIKE여야 한다.", type, "LIKE");
+    }
+
+    @Test
+    public void findEmotionType_targetId가_없는_경우_null을_반환() {
+        repository.save(fixture("t1", "m1", LIKE));
+
+        String type = repository.findEmotionType("t9", "m1");
+
+        assertNull("없는 targetId이므로 null이어야 한다.", type);
+    }
+
+    @Test
+    public void findEmotionType_memberId가_없는_경우_null을_반환() {
+        repository.save(fixture("t1", "m1", LIKE));
+
+        String type = repository.findEmotionType("t1", "m9");
+
+        assertNull("없는 memberId이므로 null이어야 한다.", type);
+    }
+
+    // -----------------------------
+    private Emotion fixture(String targetId, String memberId, EmotionType type) {
+        return new Emotion(targetId, memberId, type);
+    }
+}
