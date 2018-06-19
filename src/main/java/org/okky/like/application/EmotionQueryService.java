240ea6c3c5ef32a6fab52e266642465c48fa3556
hyunsu <coding8282@gmail.com>
2018-06-19 11:29:51 +0900

    쿼리응용서비스 기능 추가
    - 나의 공감표현 통계식을 가져오는 메소드 추가.

--- src/main/java/org/okky/like/application/EmotionQueryService.java
+++ src/main/java/org/okky/like/application/EmotionQueryService.java
@@ -5,6 +5,7 @@ import lombok.experimental.FieldDefaults;
 import org.okky.like.domain.repository.EmotionRepository;
 import org.okky.like.resource.dto.EmotionFullStatDto;
 import org.okky.like.resource.dto.EmotionStatDto;
+import org.okky.like.resource.dto.MyEmotionStatDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,11 @@ import static lombok.AccessLevel.PRIVATE;
 @FieldDefaults(level = PRIVATE)
 public class EmotionQueryService {
     EmotionRepository repository;
+
+    public MyEmotionStatDto findMyEmotionStat(String memberId) {
+        Object[] tuple = (Object[]) repository.queryMyEmotionStat(memberId);
+        return new MyEmotionStatDto(tuple);
+    }
 
     public EmotionStatDto findEmotionStat(String targetId) {
         Object[] tuple = (Object[]) repository.queryEmotionStatByTargetId(targetId);
