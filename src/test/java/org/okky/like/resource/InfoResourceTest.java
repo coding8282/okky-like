e653130c1e698349ed9b07530c1d3e6f9d826a44
hyunsu <coding8282@gmail.com>
2018-06-26 19:43:16 +0900

    테스트 리팩토링

--- src/test/java/org/okky/like/resource/InfoResourceTest.java
+++ src/test/java/org/okky/like/resource/InfoResourceTest.java
@@ -1,5 +1,6 @@
 package org.okky.like.resource;
 
+import lombok.experimental.FieldDefaults;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.okky.like.TestMother;
@@ -8,21 +9,14 @@ import org.okky.like.application.EmotionQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static lombok.AccessLevel.PRIVATE;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = InfoResource.class)
+@FieldDefaults(level = PRIVATE)
 public class InfoResourceTest extends TestMother {
     @Autowired
     MockMvc mvc;
@@ -34,22 +28,7 @@ public class InfoResourceTest extends TestMother {
     EmotionApplicationService EmotionApplicationService;
 
     @Test
-    public void name() throws Exception {
-        mvc.perform(get("/").content("message").with(user("admin1").roles("ADMIN")))
-                .andExpect(status().isOk())
-                .andExpect(content().string("aaa"))
-                .andDo(print());
-        System.out.println(mvc);
-    }
-
-    @Test
-    @WithMockUser
-    public void name2() throws Exception {
-        when(EmotionQueryService.queryEmotionFullStat("1", "2")).thenReturn(null);
-
-        mvc.perform(get("/articles/1/emotions?memberId=2").contentType(MediaType.TEXT_PLAIN).content("message"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("okky-like service333")));
+    public void name() {
+        // Empty
     }
 }
