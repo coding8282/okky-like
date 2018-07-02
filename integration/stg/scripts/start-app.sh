ca2ab593837ab7e77a679666cee24ce20fcdc829
hyunsu <coding8282@gmail.com>
2018-07-02 20:20:00 +0900

    배포스크립트 수정
    - nohup 대신 service로 시작하게끔 수정.

--- integration/stg/scripts/start-app.sh
+++ integration/stg/scripts/start-app.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-nohup java -jar \
-/home/ec2-user/okky-like-1.0.0.jar \
---spring.cloud.config.label=develop \
-> /dev/null 2> /dev/null < /dev/null &
+sudo ln -sf /home/ec2-user/okky-like-1.0.0-SNAPSHOT.jar /etc/init.d/okky-like
+sudo service okky-like start
